9d84108d539d890981f20078c28976d073c3c2dd
Glitch (paul-profile) <none>
2021-07-04 17:30:25 +0000

    Contact me links updated

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -26,41 +26,46 @@ const Contact = function() {
         <br />
         <div className="title">Contact Me:</div>
         <p>
-          <a
-            className="readable link"
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.linkedin.com/in/paul-o-connell-604"
-            title="LinkedIn"
-          >
-            <img src="https://res.cloudinary.com/pollie/image/upload/c_thumb,h_102,w_131/v1622842617/linked-inLogo_arbzyw.webp" />
-            <br />
-            www.linkedin.com/in/paul-o-connell-604
-          </a>
+          <div className="border">
+            <a
+              className="readable link border"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.linkedin.com/in/paul-o-connell-604"
+              title="LinkedIn"
+            >
+              <img src="https://res.cloudinary.com/pollie/image/upload/c_thumb,h_102,w_131/v1622842617/linked-inLogo_arbzyw.webp" />
+              <br />
+              www.linkedin.com/in/paul-o-connell-604
+            </a>
+          </div>
           <br />
           <br />
-          <a
-            className="readable link"
-            target="_blank"
-            href="https://github.com/pauloconnell"
-            rel="noopener noreferrer"
-            title="GitHub Repos"
-          >
-            <img src="https://res.cloudinary.com/pollie/image/upload/c_thumb,g_face,w_118/v1622847396/GitHub-Mark_hnhn3m.png" />
-            <br />
-            https://github.com/pauloconnell
-          </a>
-
+          <div className="border">
+            <a
+              className="readable link border"
+              target="_blank"
+              href="https://github.com/pauloconnell"
+              rel="noopener noreferrer"
+              title="GitHub Repos"
+            >
+              <img src="https://res.cloudinary.com/pollie/image/upload/c_thumb,g_face,w_118/v1622847396/GitHub-Mark_hnhn3m.png" />
+              <br />
+              https://github.com/pauloconnell
+            </a>
+          </div>
           <br />
           <br />
-          <a
-            className="readable link"
-            href="mailto:p_ollie@hotmail.com?Subject=FromProfile"
-            target="_blank"
-            title="p_ollie@hotmail.com"
-          >
-            Click here to Email Me
-          </a>
+          <div className="border">
+            <a
+              className="readable link border"
+              href="mailto:p_ollie@hotmail.com?Subject=FromProfile"
+              target="_blank"
+              title="p_ollie@hotmail.com"
+            >
+              Click here to Email Me
+            </a>
+          </div>
         </p>
         <br />
         <br />
